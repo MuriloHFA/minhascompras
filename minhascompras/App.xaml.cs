517582458df69b6aace840f2dfe1f15c85b40502
murilo hfa <murilohenriquef10@gmail.com>
2025-03-10 02:10:37 -0300

    agenda 3

--- minhascompras/App.xaml.cs
+++ minhascompras/App.xaml.cs
@@ -1,7 +1,29 @@
-﻿namespace minhascompras
+﻿using minhascompras.Helpers;
+
+namespace minhascompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if(_db == null)
+                {
+                    string patch = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData), 
+                        "banco_sqlites_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(patch);
+                }
+
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
