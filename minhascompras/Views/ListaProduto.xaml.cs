b6347c45417284d1ef08a42c09b943ecdfc86793
murilo hfa <murilohenriquef10@gmail.com>
2025-03-23 14:41:20 -0300

    correção

--- minhascompras/Views/ListaProduto.xaml.cs
+++ minhascompras/Views/ListaProduto.xaml.cs
@@ -5,39 +5,40 @@ namespace minhascompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
-	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 
-	public ListaProduto()
-	{
+    public ListaProduto()
+    {
         InitializeComponent();
 
-		lst_produtos.ItemsSource = lista;
-	}
+        lst_produtos.ItemsSource = lista;
+    }
 
     protected async override void OnAppearing()
     {
-		List<Produto> tmp = await App.Db.Getall();
+        List<Produto> tmp = await App.Db.Getall();
 
-		tmp.ForEach(i => lista.Add(i));
+        tmp.ForEach(i => lista.Add(i));
     }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
-		try
-		{
-			Navigation.PushAsync(new Views.NovoProduto());
+        try
+        {
+            Navigation.PushAsync(new Views.NovoProduto());
 
-		} catch (Exception ex)
-		{
-			DisplayAlert("ops", ex.Message, "OK");
-		}
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("ops", ex.Message, "OK");
+        }
     }
 
     private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
     {
-		string q = e.NewTextValue;
+        string q = e.NewTextValue;
 
-		lista.Clear();
+        lista.Clear();
 
         List<Produto> tmp = await App.Db.Search(q);
 
@@ -46,11 +47,11 @@ public partial class ListaProduto : ContentPage
 
     private void ToolbarItem_Clicked_1(object sender, EventArgs e)
     {
-		double soma = lista.Sum(i => i.Total);
+        double soma = lista.Sum(i => i.Total);
 
-		string msg = $"O total é {soma:C}";
+        string msg = $"O total ? {soma:C}";
 
-		DisplayAlert ("Total dos Produtos", msg, "OK");
+        DisplayAlert("Total dos Produtos", msg, "OK");
     }
 
     private void MenuItem_Clicked(object sender, EventArgs e)
